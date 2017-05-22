9428885e33df370be17f16e952118776c06e54c7
Quentin <q.roche@hotmail.fr>
2017-05-22 16:13:39 +0200

    commit opti Input - PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
 
 	public float speed;
 	public float tilt;
@@ -19,17 +20,20 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rb.velocity = movement * speed;
+		if (moveVertical != 0 || moveHorizontal != 0) 
+		{
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.velocity = movement * speed;
 
-		rb.position = new Vector3 (
-			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
-			0.0f,
-			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
-		);
+			rb.position = new Vector3 (
+				Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+				0.0f,
+				Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+			);
 
-		rb.rotation = Quaternion.Euler (0.0f, 0.0f , rb.velocity.x * -tilt);
+			rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+		}
 	}
 }
